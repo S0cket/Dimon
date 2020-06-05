f40e60b0bfa328426bd794603543b1b285693a48
S0cket <rus.swordsman@gmail.com>
2020-06-05 04:56:22 +0400

    создал 18

---
+++ 18/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int n1, n2[6];
+
+int main(void) {
+	ifstream file("f.txt");
+	char c;
+	bool flag = false;
+	
+	while (file.get(c)) {
+		if (c == 'a')
+			flag = true;
+		else if (flag && c == 'b') {
+			n1 ++;
+			flag = false;
+		}
+		else
+			flag = false;
+	}
+	file.close();
+
+	file.open("f.txt");
+	while (file >> c) {
+		int n = c - 'a';
+		if (n >= 0 && n < 6)
+			n2[n] ++;
+	}
+	file.close();
+
+	cout << "count \"ab\" = " << n1 << endl;
+	for (int i = 0; i < 6; ++ i)
+		cout << "count \"" << (char)('a' + i) << "\" = " << n2[i] << endl;
+	return 0;
+}
